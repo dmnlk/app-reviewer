5f22b23de11e3e2f92c7c1b1ce897e7e5117ece5
dmnlk <seikima2demon@gmail.com>
2015-11-03 23:48:48 +0900

    demonize program

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
-
+import (
+	"os"
+	"os/exec"
+	"time"
+	"io/ioutil"
+)
 
 func main() {
-	fmt.Println("hello")
+	if len(os.Args) == 1 {
+		cmd := exec.Command(os.Args[0], "--child")
+		cmd.Start()
+	} else {
+		for {
+			now := time.Now()
+			content := []byte(now.String())
+			ioutil.WriteFile("/tmp/output", content, os.ModePerm)
+		}
+	}
 }
