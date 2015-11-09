63a145a26115665e0b84536ca59cbd08d9613722
dmnlk <seikima2demon@gmail.com>
2015-11-10 01:35:18 +0900

    test http requesgt

--- main.go
+++ main.go
@@ -1,35 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"os"
-	"os/exec"
-	"time"
+	"net/http"
+	"github.com/k0kubun/pp"
 )
 
 var (
-	APPSTORE_ID string = "493470467"
+	APPSTORE_ID   string = "493470467"
 	PLAYSTORE_URL string = ""
 )
 
+const (
+	APPSTORE_URL string = "http://itunes.apple.com/jp/rss/customerreviews/id=%s/json"
+)
+
 func main() {
-//	if len(os.Args) == 1 {
-//		cmd := exec.Command(os.Args[0], "--child")
-//		cmd.Start()
-//	} else {
-//		tick := time.Tick(3 * time.Second)
-//		for {
-//			select {
-//			case _ = <-tick:
-//				now := time.Now()
-//				content := []byte(now.String())
-//				ioutil.WriteFile("/tmp/output", content, os.ModePerm)
-//			}
-//		}
-//	}
+	//	if len(os.Args) == 1 {
+	//		cmd := exec.Command(os.Args[0], "--child")
+	//		cmd.Start()
+	//	} else {
+	//		tick := time.Tick(3 * time.Second)
+	//		for {
+	//			select {
+	//			case _ = <-tick:
+	//				now := time.Now()
+	//				content := []byte(now.String())
+	//				ioutil.WriteFile("/tmp/output", content, os.ModePerm)
+	//			}
+	//		}
+	//	}
+	GetAppStoreReview()
 }
 
 func GetAppStoreReview() {
+	url := fmt.Sprintf(APPSTORE_URL, APPSTORE_ID)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Errorf("error")
+	}
+	defer resp.Body.Close()
+	val, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Errorf("error")
+	}
+	pp.Println(val)
 
 }
 
