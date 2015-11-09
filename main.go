598d1ef97efc207e8298de616e378075de51eefb
dmnlk <seikima2demon@gmail.com>
2015-11-10 01:28:53 +0900

    tmp comment out

--- main.go
+++ main.go
@@ -13,20 +13,20 @@ var (
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		cmd := exec.Command(os.Args[0], "--child")
-		cmd.Start()
-	} else {
-		tick := time.Tick(3 * time.Second)
-		for {
-			select {
-			case _ = <-tick:
-				now := time.Now()
-				content := []byte(now.String())
-				ioutil.WriteFile("/tmp/output", content, os.ModePerm)
-			}
-		}
-	}
+//	if len(os.Args) == 1 {
+//		cmd := exec.Command(os.Args[0], "--child")
+//		cmd.Start()
+//	} else {
+//		tick := time.Tick(3 * time.Second)
+//		for {
+//			select {
+//			case _ = <-tick:
+//				now := time.Now()
+//				content := []byte(now.String())
+//				ioutil.WriteFile("/tmp/output", content, os.ModePerm)
+//			}
+//		}
+//	}
 }
 
 func GetAppStoreReview() {
