d468d0a702034208f228a0c998bd83f6a5724f4a
dmnlk <seikima2demon@gmail.com>
2015-11-11 00:15:58 +0900

    update struct

--- Struct.go
+++ Struct.go
@@ -11,6 +11,51 @@ type ITunesResponse struct {
 			} `json:"uri"`
 		} `json:"author"`
 		Entry []struct {
+			Author struct {
+				Label string `json:"label"`
+				Name  struct {
+					Label string `json:"label"`
+				} `json:"name"`
+				Uri struct {
+					Label string `json:"label"`
+				} `json:"uri"`
+			} `json:"author"`
+			Content struct {
+				Attributes struct {
+					Type string `json:"type"`
+				} `json:"attributes"`
+				Label string `json:"label"`
+			} `json:"content"`
+			ID struct {
+				Label string `json:"label"`
+			} `json:"id"`
+			Im_ContentType struct {
+				Attributes struct {
+					Label string `json:"label"`
+					Term  string `json:"term"`
+				} `json:"attributes"`
+			} `json:"im:contentType"`
+			Im_Rating struct {
+				Label string `json:"label"`
+			} `json:"im:rating"`
+			Im_Version struct {
+				Label string `json:"label"`
+			} `json:"im:version"`
+			Im_VoteCount struct {
+				Label string `json:"label"`
+			} `json:"im:voteCount"`
+			Im_VoteSum struct {
+				Label string `json:"label"`
+			} `json:"im:voteSum"`
+			Link struct {
+				Attributes struct {
+					Href string `json:"href"`
+					Rel  string `json:"rel"`
+				} `json:"attributes"`
+			} `json:"link"`
+			Title struct {
+				Label string `json:"label"`
+			} `json:"title"`
 			Category struct {
 				Attributes struct {
 					Im_Id  string `json:"im:id"`
@@ -19,25 +64,14 @@ type ITunesResponse struct {
 					Term   string `json:"term"`
 				} `json:"attributes"`
 			} `json:"category"`
-			ID struct {
-				Attributes struct {
-					Im_BundleId string `json:"im:bundleId"`
-					Im_Id       string `json:"im:id"`
-				} `json:"attributes"`
-				Label string `json:"label"`
-			} `json:"id"`
+
 			Im_Artist struct {
 				Attributes struct {
 					Href string `json:"href"`
 				} `json:"attributes"`
 				Label string `json:"label"`
 			} `json:"im:artist"`
-			Im_ContentType struct {
-				Attributes struct {
-					Label string `json:"label"`
-					Term  string `json:"term"`
-				} `json:"attributes"`
-			} `json:"im:contentType"`
+
 			Im_Image []struct {
 				Attributes struct {
 					Height string `json:"height"`
@@ -60,19 +94,10 @@ type ITunesResponse struct {
 				} `json:"attributes"`
 				Label string `json:"label"`
 			} `json:"im:releaseDate"`
-			Link struct {
-				Attributes struct {
-					Href string `json:"href"`
-					Rel  string `json:"rel"`
-					Type string `json:"type"`
-				} `json:"attributes"`
-			} `json:"link"`
+
 			Rights struct {
 				Label string `json:"label"`
 			} `json:"rights"`
-			Title struct {
-				Label string `json:"label"`
-			} `json:"title"`
 		} `json:"entry"`
 		Icon struct {
 			Label string `json:"label"`
